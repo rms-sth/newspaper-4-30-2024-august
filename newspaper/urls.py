2cc22b6973628c130c7725d021de8260cd765a0d
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-08-18 16:53:20 +0545

    Add home view

---
+++ newspaper/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from newspaper import views
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+]
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from newspaper.models import Post
 
-# Create your views here.
+from django.views.generic import ListView
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = "aznews/home.html"
+    context_object_name = "posts"
