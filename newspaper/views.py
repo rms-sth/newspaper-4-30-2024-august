f0812da5c4d89d3d0d0a3d611a6b35ed400bf2c8
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-08-20 18:02:04 +0545

    Add home page data

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,6 +1,9 @@
-from newspaper.models import Post
+from newspaper.models import Category, Post, Tag
 
 from django.views.generic import ListView
+from datetime import timedelta
+
+from django.utils import timezone
 
 
 class HomeView(ListView):
@@ -20,5 +23,17 @@ class HomeView(ListView):
         context["featured_posts"] = Post.objects.filter(
             published_at__isnull=False, status="active"
         ).order_by("-published_at", "-views_count")[1:4]
+
+        one_week_ago = timezone.now() - timedelta(days=7)
+        context["weekly_top_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
+        ).order_by("-published_at", "-views_count")[:7]
+
+        context["recent_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:7]
+
+        context["categories"] = Category.objects.all()[:4]
+        context["tags"] = Tag.objects.all()[:10]
 
         return context
