5e785c9962386bfdac23f4b448d2c3b43413c9ee
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-09-03 15:04:40 +0545

    Add summer note

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
-from newspaper.models import Category, Post, Tag, UserProfile, Comment, Contact
+from newspaper.models import (
+    Category,
+    Post,
+    Tag,
+    UserProfile,
+    Comment,
+    Contact,
+    Newsletter,
+)
+from django_summernote.admin import SummernoteModelAdmin
 
 
 admin.site.register(Category)
-admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(Contact)
+admin.site.register(Newsletter)
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
+
+
+admin.site.register(Post, PostAdmin)
