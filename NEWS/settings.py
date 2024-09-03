0e300a778dd7cfc59315e2fd53cdc56f976123e5
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-09-03 18:07:20 +0545

    Add drf basic app

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -31,14 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party apps
+    "rest_framework",
     "django_summernote",
+    # custom apps
     "newspaper",
+    "api",
 ]
 
 MIDDLEWARE = [
